fffeec686a85d04cc7a7e7c7405eb031b0f18a00
Gavin Schulz <gavin@inkling.com>
2012-07-21 16:58:01 -0700

    Added timestamp route handler and public folder.

--- web.js
+++ web.js
@@ -2,8 +2,19 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(request, response) {
   response.send('Hello World!');
+});
+
+app.get('/timestamp', function(request, response){
+    response.contentType('application/json');
+
+    var clientTimestamp = request.param('t');
+    var diff = (Date.now()) - clientTimestamp;
+
+    response.send(String(diff));
 });
 
 var port = process.env.PORT || 5000;
