5a9b5a112c7bc44daa8c3dfbb372039ee383f6f8
Neal Wu <nealwu@college.harvard.edu>
2012-07-21 19:53:51 -0400

    Server.py: fix spacing

--- server.py
+++ server.py
@@ -10,14 +10,14 @@ class CustomHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
     def do_GET(self):
     	print 'path is ', self.path
         if self.path=='/hello':
-        	time = time.time()
+            time = time.time()
             self.send_response(200)
             self.send_header('Content-type','text/html')
             self.end_headers()
             self.wfile.write(sayHello())
             return
         else:
-        	self.wfile.write("Error")
+            self.wfile.write("Error")
 
 httpd = SocketServer.ThreadingTCPServer((IP, PORT),CustomHandler)
 
