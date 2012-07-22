122e447191ffefc8f618bb8bda726468b44f290a
Willie Yao <yao@inkling.com>
2012-07-21 19:23:15 -0700

    implement server timestamp

--- server.py
+++ server.py
@@ -1,24 +1,30 @@
 import SocketServer
 import SimpleHTTPServer
 from datetime import datetime
+import urlparse
+import py.test
+
 PORT = 8000
 IP = ''
-def sayHello():
-    return 'Hello'
 
 class CustomHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
     def do_GET(self):
-        if self.path == '/timestamp':
+        if self.path.startswith('/timestamp'):
             time = datetime.now()
-            clientTime = self
-            #diff = time - clientTime
 
-            print dir(self.request)
+            parsed_path = urlparse.urlparse(self.path)
+            clientUnixTime = parsed_path.query.split('=')[1]
+            clientTime = datetime.fromtimestamp(int(clientUnixTime)).strftime('%Y-%m-%d %H:%M:%S')
+
+            print time
+            print clientTime
 
             self.send_response(200)
             self.send_header('Content-type','text/html')
             self.end_headers()
-            self.wfile.write(sayHello())
+            self.wfile.write(clientTime)
+        else:
+            pass
         return
 
 httpd = SocketServer.ThreadingTCPServer((IP, PORT),CustomHandler)
