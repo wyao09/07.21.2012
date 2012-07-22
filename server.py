becd99229393a08f4f4d8c146670aefd7ee79837
Gavin Schulz <gavin@inkling.com>
2012-07-22 01:49:15 -0700

    fix response code

--- server.py
+++ server.py
@@ -23,7 +23,7 @@ class CustomHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 
             diff = int(masterTime) - int(clientTime)
 
-            self.send_response(200)
+            #self.send_response(200)
             #self.send_header('Status', '200')
             self.send_header('Content-type','application/json')
             self.end_headers()
